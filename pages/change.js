ad01cff5329fb25b122fc798f732d78e60764d17
OnlyFlora321 <0nlyflora321@gmail.com>
2025-05-17 02:44:26 -0500

    Add files via upload

---
+++ pages/change.js
@@ -0,0 +1,15 @@
+function change01() {
+    document.getElementById("large").src = "../images/bear.jpg"
+}
+function change02() {
+    document.getElementById("large").src = "../images/hello-kitty.jpg"
+}
+function change03() {
+    document.getElementById("large").src = "../images/mymelody.png"
+}
+function change04() {
+    document.getElementById("large").src = "../images/paint-set.jpg"
+}
+function change05() {
+    document.getElementById("large").src = "../images/supplies.jpg"
+}
